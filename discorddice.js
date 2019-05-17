e454fea938e5d04d41ca92be2eb8a44d059914fb
Glen Blosser <gblosser@conversantmedia.com>
2019-05-17 15:48:13 -0500

    Debugging macro modifiers

--- discorddice.js
+++ discorddice.js
@@ -1538,17 +1538,21 @@ try {
 			if (macroName) {
 				macroName = macroName[1];
 			}
+			mess.reply(macroModifier);
 			if (macroModifier) {
 				macroModifier = parseInt(macroModifier[1]);
 			} else {
 				macroModifier = 0;
 			}
+			mess.reply(macroModifier);
 			if (macros[server] && macros[server][user] && macros[server][user][macroName]) {
 				var macro = macros[server][user][macroName];
 				var numDice = macro.match(/([0-9]+)[a-z]/);
+				mess.reply(numDice);
 				if (numDice) {
 					macro = macro.replace(numDice, Math.max(parseInt(numDice) + macroModifier, 1));
 				}
+				mess.reply(macro);
 				result = diceChecker(macro, mess.client);
 				mess.reply(result);
 			}
