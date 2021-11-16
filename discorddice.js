9123b249b9dd2f03190b5d4245002f546b39b179
Glen Blosser <gblosser@peak6.com>
2021-11-16 12:43:46 -0600

    Reverting to member

--- discorddice.js
+++ discorddice.js
@@ -1022,7 +1022,7 @@ try {
     };
 
     var initiativeHandler = function (message, user, mess) {
-        var guildUser = mess.channel.guild.members.fetch(user.id);
+        var guildUser = mess.guild.member(user);
         console.log(guildUser);
         var username = guildUser.nickname || user.username;
         var raw = message.substr(1).toLowerCase();
